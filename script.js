531ac920096498179ead7e5f71ea371df2a4c90d
RafalWKot <rafal.kot.poczta@gmail.com>
2018-08-08 21:28:19 +0200

    Update script.js 2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   getAllTasks();
 
   function getAllAvailableBoards(callback, callbackArgs) {
-    var requestUrl = trelloApiRoot + + 'getTrelloBoards';
+    var requestUrl = trelloApiRoot + 'getTrelloBoards';
 
     $.ajax({
       url: requestUrl,
